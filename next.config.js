eb19dfa8dbeae9ab521665df38d39c49ee1e8dc6
Tom <tom.bost.pro@gmail.com>
2022-12-23 15:14:28 +0100

    fix(App): Config Next Env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    EMAIL: process.env.EMAIL,
+    PASSWORD_EMAIL: process.env.PASSWORD_EMAIL,
+  },
 };
 
 module.exports = nextConfig;
