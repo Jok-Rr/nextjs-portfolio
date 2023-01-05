ea7480403a065d54035d6a1e304969a0420e780c
Tom <tom.bost.pro@gmail.com>
2023-01-05 10:22:37 +0100

    fix(page): 404 fix layout syst

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import { LinkButton } from "../src/components/atom/LinkButton";
+import { Heading2 } from "../src/components/atom/Heading2";
 import FourOhFourVector from "/public/vectors/404.svg";
+import { Container } from "../src/components/atom/Container";
 
 const FourOhFour = () => {
   return (
@@ -9,13 +11,26 @@ const FourOhFour = () => {
       <Head>
         <title>Page introuvable | Tom Bost</title>
       </Head>
-      <div
-        style={{ height: "calc(100vh - 200px)" }}
-        className="center flex flex-col items-center justify-center align-middle"
-      >
-        <Image src={FourOhFourVector} alt={"sqd"} className={"mb-7 w-1/2"} />
-        <LinkButton href="/">Page d&lsquo;accueil</LinkButton>
-      </div>
+      <Container className=" pt-24 text-center">
+        <Image
+          src={FourOhFourVector}
+          alt={"sqd"}
+          className={
+            "col-span-4 mb-7 md:col-span-2 md:col-start-2 lg:col-span-6 lg:col-start-4"
+          }
+        />
+        <Heading2 className="col-span-4 mb-6 lg:col-span-8 lg:col-start-3">
+          La page que vous recherchez semble introuvable
+        </Heading2>
+        <LinkButton
+          href="/"
+          classNameButton={
+            "col-span-4 lg:col-span-4 md:col-span-2 md:col-start-2 lg:col-start-5 lg:col-span-5"
+          }
+        >
+          Page d&lsquo;accueil
+        </LinkButton>
+      </Container>
     </>
   );
 };
